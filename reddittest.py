a8e749df2bf3fd59590b06bf754a950f3f09a49d
maxwatson142 <maxeww@gmail.com>
2018-10-15 11:41:45 +0100

    Now saves the info to a file

--- reddittest.py
+++ reddittest.py
@@ -1,6 +1,6 @@
 import praw
+import csv
 from config import *
-
 
 
 def findDepth(parentId, all_comments, postId):
@@ -9,6 +9,7 @@ def findDepth(parentId, all_comments, postId):
     """
     commentId = parentId
     count = 1
+    "Post id's when referenced from other objects start with the prefix t3_"
     while not(commentId.startswith('t3_')):
         for comment in all_comments:
             if comment.id in commentId:
@@ -16,16 +17,47 @@ def findDepth(parentId, all_comments, postId):
                 commentId = comment.parent_id
     return count;
 
+def findParentBody(parentId, all_comments):
+    for comment in all_comments:
+        if comment.id in parentId:
+            return comment.body
+    return null
 
 reddit = praw.Reddit(client_id=client,
                      client_secret=secret,
                      user_agent=user)
 
 print(reddit.read_only)
-
-for submission in reddit.subreddit('magictcg').new(limit=10):
-    print(submission.title)
-    all_comments = submission.comments.list()
-    for comment in all_comments:
-        print (comment.body)
-        print (findDepth(comment.parent_id, all_comments, submission.id))
+subcount = 0
+commentcount = 0
+with open('test.txt', mode='w') as reddit_file:
+    reddit_writer = csv.writer(reddit_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for submission in reddit.subreddit('videos').hot(limit=10):
+        "initialize variables for writing the post"
+        title = submission.title
+        depth = 0
+        body = submission.selftext
+        parent_body = ""
+        post_body = ""
+        subreddit = submission.subreddit
+        time = submission.created_utc
+        reddit_writer.writerow([title,depth,body,parent_body,post_body,subreddit,time])
+        "Initialize variables for writing comments"
+        post_body = submission.selftext
+        title = ""
+        all_comments = submission.comments.list()
+        for comment in all_comments:
+            body = comment.body
+            depth = findDepth(comment.parent_id, all_comments, submission.id)
+            if depth > 1:
+                parent_body = findParentBody(comment.parent_id, all_comments)
+            else:
+                parent_body = post_body
+            time = comment.created_utc
+            reddit_writer.writerow([title,depth,body,parent_body,post_body,subreddit,time])
+        subcount +=1
+        commentcount += len(all_comments)
+        print (subcount),
+        print(" Posts collected with "),
+        print(commentcount),
+        print(" comments.")
